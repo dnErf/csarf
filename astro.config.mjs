98a02710b54cad3056f78cf80246943c7126b6df
ff <frend.fernando@LIVE.COM>
2024-06-26 08:59:30 -0700

    -

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: cloudflare({
+    // use cloudflare or remove in prod
+    imageService: 'passthrough'
+  }),
+  vite: {
+    ssr: {
+      external: ['node:buffer'],
+    },
+    build: {
+      minify: false,
+    },
+  },
+});
