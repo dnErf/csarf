fd1455aaa6e2af65f1b366bbd12cc72b224c9033
ff <frend.fernando@LIVE.COM>
2024-11-14 20:14:44 -0800

    fx disable supabase auth

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,16 +2,16 @@ import { sequence } from "astro:middleware"
 import { supabase } from "@/lib/supabase/ssrClient"
 
 async function auth(context, next) {
-    let accessToken = context.cookies.get('sb-access-token')
-    let hasAccess = accessToken !== undefined && accessToken !== null && accessToken !== ''
+    // let accessToken = context.cookies.get('sb-access-token')
+    // let hasAccess = accessToken !== undefined && accessToken !== null && accessToken !== ''
 
-    if (context.url.pathname === '/auth/login' || context.url.pathname === '/~api/auth/login') {
-        return next()
-    }
+    // if (context.url.pathname === '/auth/login' || context.url.pathname === '/~api/auth/login') {
+    //     return next()
+    // }
 
-    if (!hasAccess) {
-        return context.redirect('/auth/login')
-    }
+    // if (!hasAccess) {
+    //     return context.redirect('/auth/login')
+    // }
 
     return next()
 }
